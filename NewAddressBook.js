cfee6e17cb2022acf05f71044840c4384a5d7a33
vundelayamini <vundelayamini@gmail.com>
2021-06-02 17:52:51 +0530

    [yamini]Add: Search person in city or state

--- NewAddressBook.js
+++ NewAddressBook.js
@@ -173,4 +173,19 @@ function duplicateEntry() {
         }
 });
 }
+
 duplicateEntry();
+//UC8-Search person in city or state
+function searchbyCityOrState() {
+    let inputCity = prompt("Enter city to search person : ");
+    let inputState = prompt("Enter state to search person : ");
+    addressBookArray.forEach(addressBook => {
+        if(addressBook.city == inputCity || addressBook.state == inputState) {
+            console.log("Person in city " +inputCity+ " and state " +inputState);
+        }
+    });
+    addressBookArray.filter(contact => contact.city == inputCity)
+    addressBookArray.filter(contact => contact.city == inputCity)
+    addressBookArray.forEach(contact => console.log(addressBookArray));
+}
+searchbyCityOrState();
