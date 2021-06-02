fb1ed9ebe5dcf9080ea8df5fea8bb6c212384bf8
vundelayamini <vundelayamini@gmail.com>
2021-06-02 17:56:50 +0530

    [yamini]Add: View person in city or state

--- NewAddressBook.js
+++ NewAddressBook.js
@@ -189,3 +189,14 @@ function searchbyCityOrState() {
     addressBookArray.forEach(contact => console.log(addressBookArray));
 }
 searchbyCityOrState();
+//UC9-View person in city or state in addressbook
+function viewByCityOrState() {
+    let inputCity = prompt("Enter city to search person : ");
+    let inputState = prompt("Enter state to search person : ");
+    if(addressBookArray.some(contact => contact.city == inputCity && contact.state == inputState)) {
+        console.log("Person in city " +inputCity+ " and state " +inputState);
+        }
+        addressBookArray.filter(contact => contact.city == inputCity && contact.state == inputState)
+        addressBookArray.forEach(contact => console.log(addressBookArray));
+}
+viewByCityOrState();
