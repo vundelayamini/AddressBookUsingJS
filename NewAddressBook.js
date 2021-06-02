22cb38d05e6780f69062e3201ad23ee8bf1ef131
vundelayamini <vundelayamini@gmail.com>
2021-06-02 18:50:07 +0530

    [yamini]Add: sort the entries in address book in city state or zip

--- NewAddressBook.js
+++ NewAddressBook.js
@@ -216,3 +216,16 @@ function sortByName() {
     addressBookArray.forEach(contact => console.log(contact.toString()));
 }
  sortByName();
+ //UC12-Sort the entries in address book in city or state or zip
+ function sortByCityStateOrZip(){
+    addressBookArray.sort((a,b) => a.city.localeCompare(b.city));
+    console.log("Sorted Array By City: ")
+    addressBookArray.forEach(contact => console.log(contact.toString()));
+    addressBookArray.sort((a, b) => a.state.localeCompare(b.state));
+    console.log("Sorted Array By State: ")
+    addressBookArray.forEach(contact => console.log(contact.toString()));
+    ddressBookArray.sort((a, b) => a.zip.localeCompare(b.zip));
+    console.log("Sorted Array By Zip: ")
+    addressBookArray.forEach(contact => console.log(contact.toString()));
+}
+sortByCityStateOrZip();
